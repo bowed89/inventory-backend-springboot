5c4ee0ee4824b9d86d9f7338eb8739ca4414c537
bowed89 <jesus.lopez.averanga@gmail.com>
2023-01-24 20:08:42 -0400

    Agregar servicio de Actualizar

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,29 +20,53 @@ public class CategoryRestController {
 
 	@Autowired
 	private ICategoryService service;
-	
-	// Obtener todas las categorias
+
+	/**
+	 * Obtener todas las categorias
+	 * @return
+	 */
 	@GetMapping("/categories")
 	public ResponseEntity<CategoryResponseRest> searchCategories() {
-		
-		 ResponseEntity<CategoryResponseRest> response = service.search();
-		 return response; 
+
+		ResponseEntity<CategoryResponseRest> response = service.search();
+		return response;
 	}
-	
-	// Obtener categorias por ID
+
+	/**
+	 * Obtener categorias por ID
+	 * @param id
+	 * @return
+	 */
 	@GetMapping("/categories/{id}")
 	public ResponseEntity<CategoryResponseRest> searchCategoriesById(@PathVariable Long id) {
-		
-		 ResponseEntity<CategoryResponseRest> response = service.searchById(id);
-		 return response; 
+
+		ResponseEntity<CategoryResponseRest> response = service.searchById(id);
+		return response;
 	}
-	
-	// Almacenar categorias
-		@PostMapping("/categories")
-		public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category) {
-			
-			 ResponseEntity<CategoryResponseRest> response = service.save(category);
-			 return response; 
-		}
+
+	/**
+	 * Almacenar categorias
+	 * @param category
+	 * @return
+	 */
+	@PostMapping("/categories")
+	public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category) {
+
+		ResponseEntity<CategoryResponseRest> response = service.save(category);
+		return response;
+	}
+
+	/**
+	 * Actualizar categorias
+	 * @param category
+	 * @param id
+	 * @return
+	 */
+	@PutMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> update(@RequestBody Category category, @PathVariable Long id) {
+
+		ResponseEntity<CategoryResponseRest> response = service.update(category, id);
+		return response;
+	}
 
 }
--- src/main/java/com/company/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -98,6 +98,47 @@ public class CategoryServiceImpl implements ICategoryService{
 		
 		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
 	}
+
+	@Override
+	@Transactional
+	public ResponseEntity<CategoryResponseRest> update(Category category, Long id) {
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		
+		try {
+			
+			Optional<Category> categorySearch = categoryDao.findById(id);
+			
+			if(categorySearch.isPresent()) {
+				// Si no esta vacio, se actualiza
+				categorySearch.get().setName(category.getName());
+				categorySearch.get().setDescription(category.getDescription());
+				
+				Category categoryToUpdate = categoryDao.save(categorySearch.get());
+				
+				if(categoryToUpdate != null) {
+					list.add(categoryToUpdate);
+					response.getCategoryResponse().setCategory(list);
+					response.setMetadata("Respuesta ok", "00", "Categoria actualizada");
+					
+				} else {
+					response.setMetadata("Respuesta nok", "-1", "Categoria no actualizada");
+					return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.NOT_FOUND);
+				}
+				
+			} else {
+				response.setMetadata("Respuesta nok", "-1", "Categoria no encontrada");
+				return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.NOT_FOUND);
+			}
+			
+		} catch (Exception e) {
+			response.setMetadata("Respuesta nok", "-1", "Error al actualizar categoria");
+			e.getStackTrace();
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+	}
 		
 
 }
